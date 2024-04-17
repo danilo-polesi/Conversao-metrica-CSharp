e2501b7d860f2fcb5b540dce70b0f60b8d8ca712
danilo-polesi <danilopolesk8@gmail.com>
2024-04-17 17:32:29 -0300

    primeiro commit

---
+++ Conversão metrica/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Conversão_metrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double valor, cm, mm;
+
+            Console.Write("Digite um valor em metros: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+            cm = valor * 100;
+            mm = valor * 1000; 
+            //pq no escuro nao tem bugs
+
+            Console.WriteLine("O valor em centímetros é : " + cm);
+            Console.WriteLine("O valor em milímitros é : " + mm);
+            Console.ReadKey();
+        }
+    }
+}
